17ffdf9ab4e81888dfcc5e1542b95c61c452c3b1
Mykola <kolya.dudla@gmail.com>
2022-02-06 13:42:54 +0200

    Init
    Init project.
    Added base structure project.
    Added nav bar component.
    Added WarehouseListComponent component.
    Added WarehouseServiceService.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { WarehouseListComponent } from './components/warehouse-list/warehouse-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    WarehouseListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
